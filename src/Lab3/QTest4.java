64f05984527e54f546ed02f3d48fc2eb1797ef5f
DhangarNikita <dhangarnikita13@gmail.com>
2025-05-12 17:14:25 +0530

    first commit

---
+++ src/Lab3/QTest4.java
@@ -0,0 +1,13 @@
+package Lab3;
+import LabEx3.Q3;
+import LabEx3.Q4;
+
+public class QTest4 extends Q4 {
+    public static void main(String[] args) {
+        QTest4 qt = new QTest4();
+        // System.out.println(qt.aa);   //not accessible its private
+        System.out.println(qt.bb);
+        //System.out.println(qt.cc);
+        System.out.println(qt.dd);
+    }
+}
---
+++ src/LabEx3/Q3.java
@@ -0,0 +1,19 @@
+package LabEx3;
+//Accessing above 4 members from another class program within the same package.
+public class Q3 {
+    private int a=10;
+    public int b=20;
+    int c = 30;
+    protected int d=40;
+
+}
+
+class QTest3{
+    public static void main(String[] args) {
+        Q3 q = new Q3();
+       // System.out.println(q.a);   //not accessible its private
+        System.out.println(q.b);
+        System.out.println(q.c);
+        System.out.println(q.d);
+    }
+}
